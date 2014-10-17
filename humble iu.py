45fec6c45e074a166f98eab999ec67b60eb2948b
SeanMelant <colgatetoothpasteyum@gmail.com>
2014-10-17 13:18:12 -0500

    fghgfhg

---
+++ humble iu.py
@@ -0,0 +1,26 @@
+import random
+
+print "Michael Bay emerges from his basement!"
+playerHealth = 113
+MichaelBayHealth = 666
+SplooshDmg = "splooshes everywhere"
+HairAttackDmg = MichaelBayHealth - (random.randint (33,56))
+BslapDmg = MichaelBayHealth - 50
+GunDmg = MichaelBayHealth - 1 or MichaelBayHealth - 0
+print "you have 113 health"
+print "the evil lord has 666 health"
+import random
+damageByMichaelBay = random.randint (9,11) or 'transformers sequel'
+
+
+UserInput == int(raw_input())
+while PlayerHealth == 0
+    if (UserInput == 1):
+        print SplooshDmg
+    if (userInput == 2):
+        MichaelBayHealth - (random.randint (33,56))
+        print "Michael Bay loses health"
+    if PlayerHealth == 0:
+        print "you are dead, your family is dead, your moms a freaking slut, your dogs been skinned alive, your friends are druggies, get over it, GAME OVER"
+    if MichaelBayHealth == 0:
+        print "The demon has been slayed, the world has been saved, everyone is now gay, hooray"
